2b457d8834f4134e604f5e31a663882392bbd45d
Tahmid <tahmidfarhan12@gmail.com>
2022-07-14 00:38:19 +0600

    add page header component

--- src/app/shared/shared-components/shared-components.module.ts
+++ src/app/shared/shared-components/shared-components.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { MaterialModule } from '../material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 const exports: any = [] ;
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageHeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule
   ], exports: exports
 })
 export class SharedComponentsModule { }
