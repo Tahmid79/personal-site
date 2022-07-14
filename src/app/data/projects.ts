bb8c9a3332623429e2fa48f0f8e69543849573ff
Tahmid <tahmidfarhan12@gmail.com>
2022-07-15 01:58:12 +0600

    project data

---
+++ src/app/data/projects.ts
@@ -0,0 +1,17 @@
+export interface IProject{
+    name : string ;
+    image : string ;
+    description : string ;
+}
+
+export const Projects: IProject[] = [
+    { 'name': 'card', 'image': 'card.JPG', 'description': 'This is the project description' },
+    { 'name': 'comp', 'image': 'comp.JPG', 'description': 'This is the project description' },
+    { 'name': 'epi', 'image': 'epi.JPG', 'description': 'This is the project description' },
+    { 'name': 'glry', 'image': 'glry.JPG', 'description': 'This is the project description' },
+    { 'name': 'img_srch', 'image': 'img_srch.PNG', 'description': 'This is the project description' },
+    { 'name': 'maps', 'image': 'maps.jpg', 'description': 'This is the project description' },
+    { 'name': 'podcast', 'image': 'podcast.JPG', 'description': 'This is the project description' },
+    { 'name': 'uinf', 'image': 'uinf.JPG', 'description': 'This is the project description' },
+    { 'name': 'video_search', 'image': 'video_search.PNG', 'description': 'This is the project description' }
+];
