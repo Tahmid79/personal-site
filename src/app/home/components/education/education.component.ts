e740b59c5e66a0447c7c45267456162f852fe297
Tahmid <tahmidfarhan12@gmail.com>
2022-08-02 01:33:35 +0600

    homepage dissertation

--- src/app/home/components/education/education.component.ts
+++ src/app/home/components/education/education.component.ts
@@ -9,6 +9,10 @@ export class EducationComponent implements OnInit {
   education_title = 'Education' ;
   degree = 'BSc(Honours) Computer Science' ;
   accreditation = '* Course accredited by British Computer Society' ;
+
+  undergad_dissertation_header = 'Undergraduate Dissertation' ;
+  undergad_dissertation_title = 'Molecular Classification of Cancer With Gene Expression Monitoring' ;
+
   constructor() { }
 
   ngOnInit(): void {
