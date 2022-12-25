5210ada58d6f5a8c263131a7cd66ad1b297704ae
Tahmid <tahmidfarhan12@gmail.com>
2022-12-25 18:24:28 +0600

    mobile health game github

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -8,9 +8,9 @@ export interface IProject{
 }
 
 export const Projects: IProject[] = [
+    { 'name': 'epi', 'image': 'epi.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : 'https://github.com/Tahmid79/Mobile-Health-Game' , 'demoLink' : 'demo' },
     { 'name': 'card', 'image': 'card.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
     { 'name': 'comp', 'image': 'comp.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
-    { 'name': 'epi', 'image': 'epi.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
     { 'name': 'glry', 'image': 'glry.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
     { 'name': 'img_srch', 'image': 'img_srch.PNG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
     { 'name': 'maps', 'image': 'maps.jpg', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
