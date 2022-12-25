af80bdade49279173add3663d78cfbe3e119bb62
Tahmid <tahmidfarhan12@gmail.com>
2022-12-25 16:39:39 +0600

    add tech stack to project data

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -2,16 +2,17 @@ export interface IProject{
     name : string ;
     image : string ;
     description : string ;
+    technology : string[] ;
 }
 
 export const Projects: IProject[] = [
-    { 'name': 'card', 'image': 'card.JPG', 'description': 'This is the project description' },
-    { 'name': 'comp', 'image': 'comp.JPG', 'description': 'This is the project description' },
-    { 'name': 'epi', 'image': 'epi.JPG', 'description': 'This is the project description' },
-    { 'name': 'glry', 'image': 'glry.JPG', 'description': 'This is the project description' },
-    { 'name': 'img_srch', 'image': 'img_srch.PNG', 'description': 'This is the project description' },
-    { 'name': 'maps', 'image': 'maps.jpg', 'description': 'This is the project description' },
-    { 'name': 'podcast', 'image': 'podcast.JPG', 'description': 'This is the project description' },
-    { 'name': 'uinf', 'image': 'uinf.JPG', 'description': 'This is the project description' },
-    { 'name': 'video_search', 'image': 'video_search.PNG', 'description': 'This is the project description' }
+    { 'name': 'card', 'image': 'card.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] },
+    { 'name': 'comp', 'image': 'comp.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] },
+    { 'name': 'epi', 'image': 'epi.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] },
+    { 'name': 'glry', 'image': 'glry.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] },
+    { 'name': 'img_srch', 'image': 'img_srch.PNG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] },
+    { 'name': 'maps', 'image': 'maps.jpg', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] },
+    { 'name': 'podcast', 'image': 'podcast.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] },
+    { 'name': 'uinf', 'image': 'uinf.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] },
+    { 'name': 'video_search', 'image': 'video_search.PNG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] }
 ];
