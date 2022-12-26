fc5a35ad79fcfffb5e59e9e46018a49c7df125c2
Tahmid <tahmidfarhan12@gmail.com>
2022-12-26 23:35:56 +0600

    skills data

--- src/app/data/skills.ts
+++ src/app/data/skills.ts
@@ -4,21 +4,21 @@ export interface ISkill{
 }
 
 export const Skills: ISkill[] = [
-    { 'name': 'Android', 'image': 'android.png' },
     { 'name': 'Angular', 'image': 'ang.jpg' },
-    { 'name': 'C', 'image': 'c.png' },
-    { 'name': 'CSS', 'image': 'css.jpg' },
-    { 'name': 'Django', 'image': 'django.jpg' },
-    { 'name': 'HTML', 'image': 'html.jpg' },
-    { 'name': 'Java', 'image': 'java.png' },
-    { 'name': 'Javascript', 'image': 'js.jpg' },
-    { 'name': 'Keras', 'image': 'keras.jpg' },
-    { 'name': 'Node.js', 'image': 'node.jpg' },
-    { 'name': 'Numpy', 'image': 'numpy.png' },
+    { 'name': 'Golang', 'image': 'golang.png' },
+    { 'name': 'TypeScript', 'image': 'typescript.png' },
     { 'name': 'Python', 'image': 'python.jpg' },
-    { 'name': 'Python', 'image': 'python.png' },
-    { 'name': 'React', 'image': 'rct.jpg' },
-    { 'name': 'Redux', 'image': 'redux.jpg' },
-    { 'name': 'Sklearn', 'image': 'sklearn.jpg' }
+    { 'name': 'Javascript', 'image': 'js.jpg' },
+    { 'name': 'Java', 'image': 'java.png' },
+    { 'name': 'C', 'image': 'c.png' },
+    { 'name': 'Haskell', 'image': 'haskell.png' },
+    { 'name': 'Android', 'image': 'android.png' },
+    { 'name': 'Matlab', 'image': 'matlab.png' },
+    { 'name': 'Keras', 'image': 'keras.jpg' },
+    { 'name': 'Sklearn', 'image': 'sklearn.jpg' },
+    { 'name': 'Numpy', 'image': 'numpy.png' },
+    { 'name': 'Node.js', 'image': 'node.jpg' },
+    { 'name': 'CSS', 'image': 'css.jpg' },
+    { 'name': 'HTML', 'image': 'html.jpg' },
 ]
 
