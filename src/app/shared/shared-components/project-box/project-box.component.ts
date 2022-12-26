7b4aba670f917d95a218d85eecb2d50c90010b62
Tahmid <tahmidfarhan12@gmail.com>
2022-12-27 00:54:42 +0600

    home project segment responsive issues

---
+++ src/app/shared/shared-components/project-box/project-box.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IProject } from '../../../data/projects';
+import { Skills } from '../../../data/skills';
+
+@Component({
+  selector: 'app-project-box',
+  templateUrl: './project-box.component.html',
+  styleUrls: ['./project-box.component.scss']
+})
+export class ProjectBoxComponent implements OnInit {
+
+  @Input() projectInfo: IProject =  { 'name': 'card', 'image': 'card.JPG', 'description': 'This is the project description', 'technology' : [], 'githubLink' : '' , 'demoLink' : '' } ;
+  projectDir = '/assets/images/projects/' ;
+  linkDir = '/assets/images/' ;
+  skillsDir = '/assets/images/tech stack old/' ;
+
+  skills = Skills ;
+  tech = 'Technology' ;
+
+  details = 'This is the project description.' ;
+  linkTitle = 'Links' ;
+
+  links = ['github.png' , 'demo.png'] ;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getImageCss(image: string, dir: string) : string {
+    const url = dir + image ;
+    const imageCss =  `url("${url}")` ;
+    return imageCss ;
+  }
+
+  getTechStack(stack: string[]): string {
+    let result = '' ;
+    for(let i = 0 ; i < stack.length ; i++){
+      const item = stack[i] ;
+      if(i == stack.length - 1 ){
+        result += item ;
+      }else{
+        result += item + ', '
+      }
+    }
+    return result ;
+  }
+
+  openLink(link : string){
+    window.open(link, '_blank');
+  }
+
+  isLinkValid(link : string) : boolean {
+    return link !== null && link !== '' ;
+  }
+
+}
--- src/app/shared/shared-components/shared-components.module.ts
+++ src/app/shared/shared-components/shared-components.module.ts
@@ -5,13 +5,15 @@ import { MaterialModule } from '../material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { SkillComponent } from './skill/skill.component';
+import { ProjectBoxComponent } from './project-box/project-box.component';
 
-const exports: any = [PageHeaderComponent, SkillComponent] ;
+const exports: any = [PageHeaderComponent, SkillComponent, ProjectBoxComponent] ;
 
 @NgModule({
   declarations: [
     PageHeaderComponent,
-    SkillComponent
+    SkillComponent,
+    ProjectBoxComponent
   ],
   imports: [
     CommonModule,
