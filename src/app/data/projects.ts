149672e17080fb0c6da30979e3ecd941d0a7171b
Tahmid <tahmidfarhan12@gmail.com>
2022-12-26 22:15:33 +0600

    new project data

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -8,13 +8,9 @@ export interface IProject{
 }
 
 export const Projects: IProject[] = [
-    { 'name': 'epi', 'image': 'epi.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : 'https://github.com/Tahmid79/Mobile-Health-Game' , 'demoLink' : 'demo' },
-    { 'name': 'card', 'image': 'card.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
-    { 'name': 'comp', 'image': 'comp.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
-    { 'name': 'glry', 'image': 'glry.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
-    { 'name': 'img_srch', 'image': 'img_srch.PNG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
-    { 'name': 'maps', 'image': 'maps.jpg', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
-    { 'name': 'podcast', 'image': 'podcast.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
-    { 'name': 'uinf', 'image': 'uinf.JPG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' },
-    { 'name': 'video_search', 'image': 'video_search.PNG', 'description': 'This is the project description', 'technology' : ['Angular', 'Javascript', 'Typescript', 'Cypress', 'Angular Material'] , 'githubLink' : '' , 'demoLink' : '' }
+    { 'name': 'Eco Thinkers', 'image': 'ecothinkers.png', 'description': 'Website for environmental consultancy Eco Thinkers showing the services provided and the projects completed by the organization', 'technology' : ['Angular', 'Angular Material','Typescript','Javascript','FlexLayout','Firebase','Circle CI'] , 'githubLink' : '' , 'demoLink' : 'https://www.ecothinkers.net/' },
+    { 'name': 'Mobile Health Game On Epilepsy', 'image': 'epi.JPG', 'description': 'Game to help spread awareness about epilepsy', 'technology' : ['Unity' , 'C#' , 'Mixamo Models'] , 'githubLink' : 'https://github.com/Tahmid79/Mobile-Health-Game' , 'demoLink' : '' },
+    { 'name': 'Android Running App', 'image': 'maps.jpg', 'description': 'Android app that can show the path of the user while running and record track data', 'technology' : ['Android', 'Google Maps API', 'Java'] , 'githubLink' : 'https://github.com/Tahmid79/Android-Running-App' , 'demoLink' : '' },
+    { 'name': 'Propel Your Business', 'image': 'propelBusiness.png', 'description': 'Restaurant landing page', 'technology' : ['Angular', 'Ng Zorro','Typescript','Javascript','FlexLayout','Firebase'] , 'githubLink' : '' , 'demoLink' : 'https://propel-business.web.app/' },
+    { 'name': 'Business Starter', 'image': 'businessStarter.png', 'description': 'Startup landing page', 'technology' : ['Angular', 'Angular Material','Typescript','Javascript','FlexLayout'] , 'githubLink' : 'https://github.com/Tahmid79/Business-Starter' , 'demoLink' : 'https://tahmid-business-starter.netlify.app/' }
 ];
